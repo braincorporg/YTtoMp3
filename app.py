f8f101bef2870bb63bd075b752e25fcbee19a811
Kevin Lopez <104493450+Braingearceo@users.noreply.github.com>
2024-02-05 16:03:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import os
-import subprocess
+import pafy
 from flask import Flask, request, jsonify, send_file
-from moviepy.editor import *
 from openai import OpenAI
 
 app = Flask(__name__)
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-def download_audio_youtube_dl(youtube_url, output_filename):
-    # Construct the youtube-dl command
-    command = ["youtube-dl", "-x", "--audio-format", "mp3", "-o", output_filename, youtube_url]
-    subprocess.run(command, check=True)
+def download_audio_pafy(youtube_url, output_filename):
+    video = pafy.new(youtube_url)
+    best_audio = video.getbestaudio(preftype="m4a")
+    best_audio.download(filepath=output_filename)
 
 def get_transcript(file_path):
     client = OpenAI(api_key=OPENAI_API_KEY)
@@ -33,10 +32,10 @@ def download_mp3():
     if not youtube_url:
         return "YouTube URL is required", 400
 
-    output_filename = "temp_audio.mp3"
-    download_audio_youtube_dl(youtube_url, output_filename)
+    output_filename = "temp_audio.m4a"
+    download_audio_pafy(youtube_url, output_filename)
     
-    return send_file(output_filename, mimetype="audio/mp3", as_attachment=True, download_name="download.mp3")
+    return send_file(output_filename, mimetype="audio/mp4", as_attachment=True, download_name="download.m4a")
 
 @app.route('/transcribe', methods=['GET'])
 def transcribe():
@@ -45,8 +44,8 @@ def transcribe():
         return "YouTube URL is required", 400
 
     try:
-        output_filename = "temp_audio.mp3"
-        download_audio_youtube_dl(youtube_url, output_filename)
+        output_filename = "temp_audio.m4a"
+        download_audio_pafy(youtube_url, output_filename)
         transcript = get_transcript(output_filename)
 
         if transcript:
